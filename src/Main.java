3e49ecfeba26ed81f39e19c4348009a69e50b9d9
chloetohce <chloetohce@gmail.com>
2024-10-04 15:41:00 +0800

    testing gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,53 @@
 import java.io.Console;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main{
+
+    public static void printMenu() {
+        System.out.println("===================== MENU =====================");
+        System.out.println("list: Display all items in your cart. ");
+        System.out.println("add <items>: Add listed items to your cart. ");
+        System.out.println("delete <num>: Delete the item at position num.");
+        System.out.println("exit: Exit the program. ");
+        System.out.println("================================================");
+        System.out.println();
+        System.out.println();
+    }
+
     public static void main(String[] args) {
+        // Database
+        String db;
+        try {
+            db = args[0];
+        } catch (IndexOutOfBoundsException e) {
+            db = "db";
+        }
+        File dir = new File(db);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        // Handle Cart
         Cart cart = new Cart();
         Console console = System.console();
 
-        System.out.println("Welcome to your shopping cart");
-        while (true) {
-            String input = console.readLine("> ");
+        System.out.println("Welcome to your shopping cart!");
+        System.out.println();
+        String input = "";
+
+        while (!input.equals("exit")) {
+            printMenu();
+            input = console.readLine("> ").trim();
+
+            // List
             if (input.equals("list")) {
                 System.out.println(cart.toString());
 
+            // Add items
             } else if (input.startsWith("add")) {
-                String items = input.substring(4);
-                Scanner s = new Scanner(items).useDelimiter(", ");
-
+                String items = input.substring(4).replace(",", " ");
+                Scanner s = new Scanner(items).useDelimiter(" ");
                 while (s.hasNext()) {
                     String item = s.next();
                     if (cart.contains(item)) {
@@ -25,26 +57,32 @@ public class Main{
                     System.out.println(item + " added to cart");
                     }
                 }
-                
-            } else if (input.startsWith("delete")) {
-                Integer num = Integer.parseInt(input.substring(7));
-                Integer index = num - 1;
 
-                // check if num is out of index
-                if (num > cart.getLength()) {
+            // Delete item
+            } else if (input.startsWith("delete")) {
+                int num = Integer.parseInt(input.substring(7));
+                int index = num - 1;
+                if (index > cart.getLength() - 1) { // checking if index is out of bounds
                     System.out.println("Incorrect item index");
                 } else {
                     String removedItem = cart.getItem(index);
                     cart = cart.remove(index);
                     System.out.println(removedItem + " removed from cart");
                 }
-            
-            } else if (input.equals("done")) {
-                System.out.println("exiting cart");
-                break;
+
+            // Exit program
+            } else if (input.equals("exit")) {
+                System.out.println("Exiting cart");
+
+            // Unrecognised command
             } else {
-                System.out.println("unrecognised command");
+                System.out.println("Unrecognised command. Please try again.");
             }
         }
+
+        System.out.println("Exit successful.");
+        System.out.println("Goodbye!");
+        System.out.println();
+
     }
 }
